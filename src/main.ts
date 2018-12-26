f6a412db5b1891f41f1113a8e0f5f3fc8744a1ca
Jan Marthedal Rasmussen <jan@janmr.com>
2018-12-26 10:40:32 +0100

    Rewrite some of move generation

--- src/main.ts
+++ src/main.ts
@@ -5,5 +5,8 @@ const b = initBoardFromFEN('rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR');
 // const b = initBoardFromFEN('rnbqkbnr/pp1ppppp/8/2p5/4P3/5N2/PPPP1PPP/RNBQKB1R');
 showBoard(b);
 
-const moves = generateMoves(b);
-console.log(moves);
+const { moves, count } = generateMoves(b);
+
+for (let c = 0; c < count; c++) {
+    console.log(moves.subarray(c << 3, (c + 1) << 3));
+}
--- src/moves.ts
+++ src/moves.ts
@@ -10,38 +10,31 @@ import {
 // 4-8: board state xor
 export type Move = Uint8Array;
 
-const PIECE_MOVEMENTS: Array<Array<number>> = [
-    [],
-    [],                                  // PAWN
-    [9, 11, -9, -11],                    // BISHOP
-    [-21, -19, 19, 21, -12, -8, 8, 12],  // KNIGHT
-    [-1, 1, -10, 10],                    // ROOK
-    [9, 11, -9, -11, -1, 1, -10, 10],    // QUEEN
-    [9, 11, -9, -11, -1, 1, -10, 10]     // KING
-]
+const BISHOP_MOVEMENTS = [9, 11, -9, -11];
+const KNIGHT_MOVEMENTS = [-21, -19, 19, 21, -12, -8, 8, 12];
+const ROOK_MOVEMENTS = [-1, 1, -10, 10];
+const KING_MOVEMENTS = [9, 11, -9, -11, -1, 1, -10, 10];
 
-export function generateMoves(board: Board): Array<Move> {
+// maximum number of moves of any position: 218 (http://www.talkchess.com/forum3/viewtopic.php?t=61792)
+
+export function generateMoves(board: Board): { moves: Uint8Array, count: number } {
     const thisTurn = board[BOARD_INDEX_TURN];
     const nextTurn = thisTurn === WHITE ? BLACK : WHITE;
+    const turnXor = thisTurn ^ nextTurn;
     // const thisCastling = board[BOARD_INDEX_CASTLING];
     const thisEP = board[BOARD_INDEX_EP];
     const thisPlys = board[BOARD_INDEX_PLYS];
-    const moves: Array<Move> = [];
+    const moves = new Uint8Array(256 * 8);
+    let length = 0;
     let pos = 21, piece: number, basePiece: number;
 
-    function addMove(to: number, nextEP: number) {
+    function addMove(to: number, nextEP: number, nextPlys: number) {
         const captured = board[to];
-        const nextPlys = captured !== EMPTY || basePiece === PAWN ? 0 : thisPlys + 1;
-        moves.push(new Uint8Array([
-            pos,
-            to,
-            captured,
-            0,
-            thisTurn ^ nextTurn,
-            0,
-            thisEP ^ nextEP,
-            thisPlys ^ nextPlys    
-        ]));
+        moves.set([
+            pos, to, captured, 0,
+            turnXor, 0, thisEP ^ nextEP, thisPlys ^ nextPlys
+        ], length);
+        length += 8;
     }
 
     for (let r = 8; r >= 1; r--) {
@@ -53,57 +46,85 @@ export function generateMoves(board: Board): Array<Move> {
                     case PAWN:
                         if (thisTurn === WHITE) {
                             if (board[pos - 10] === EMPTY) {
-                                addMove(pos - 10, 0);
+                                addMove(pos - 10, 0, 0);
                                 if (r === 2 && board[pos - 20] === EMPTY) {
-                                    addMove(pos - 20, pos - 10);
+                                    addMove(pos - 20, pos - 10, 0);
                                 }
                             }
                             if ((board[pos - 11] & COLOR_MASK) === BLACK) {
-                                addMove(pos - 11, 0);
+                                addMove(pos - 11, 0, 0);
                             }
                             if ((board[pos - 9] & COLOR_MASK) === BLACK) {
-                                addMove(pos - 9, 0);
+                                addMove(pos - 9, 0, 0);
                             }
                         } else {
                             if (board[pos + 10] === EMPTY) {
-                                addMove(pos + 10, 0);
+                                addMove(pos + 10, 0, 0);
                                 if (r === 7 && board[pos + 20] === EMPTY) {
-                                    addMove(pos + 20, pos + 10);
+                                    addMove(pos - 20, pos - 10, 0);
                                 }
                             }
                             if ((board[pos + 11] & COLOR_MASK) === WHITE) {
-                                addMove(pos + 11, 0);
+                                addMove(pos + 11, 0, 0);
                             }
                             if ((board[pos + 9] & COLOR_MASK) === WHITE) {
-                                addMove(pos + 9, 0);
+                                addMove(pos + 9, 0, 0);
                             }
                         }
+                        break;
                     case BISHOP:
-                    case ROOK:
-                    case QUEEN:
-                        {
-                            const movements = PIECE_MOVEMENTS[basePiece];
-                            for (const delta of movements) {
-                                let to = pos + delta;
-                                while (board[to] === EMPTY) {
-                                    addMove(to, 0);
-                                    to += delta;
-                                }
-                                if ((board[to] & COLOR_MASK) === nextTurn) {
-                                    addMove(to, 0);
-                                }
+                        for (const delta of BISHOP_MOVEMENTS) {
+                            let to = pos + delta;
+                            while (board[to] === EMPTY) {
+                                addMove(to, 0, thisPlys + 1);
+                                to += delta;
+                            }
+                            if ((board[to] & COLOR_MASK) === nextTurn) {
+                                addMove(to, 0, 0);
                             }
                         }
                         break;
                     case KNIGHT:
+                        for (const delta of KNIGHT_MOVEMENTS) {
+                            const to = pos + delta;
+                            if (board[to] === EMPTY) {
+                                addMove(to, 0, thisPlys + 1);
+                            } else if ((board[to] & COLOR_MASK) === nextTurn) {
+                                addMove(to, 0, 0);
+                            }
+                        }
+                        break;
+                    case ROOK:
+                        for (const delta of ROOK_MOVEMENTS) {
+                            let to = pos + delta;
+                            while (board[to] === EMPTY) {
+                                addMove(to, 0, thisPlys + 1);
+                                to += delta;
+                            }
+                            if ((board[to] & COLOR_MASK) === nextTurn) {
+                                addMove(to, 0, 0);
+                            }
+                        }
+                        break;
+                    case QUEEN:
+                        for (const delta of KING_MOVEMENTS) {
+                            let to = pos + delta;
+                            while (board[to] === EMPTY) {
+                                addMove(to, 0, thisPlys + 1);
+                                to += delta;
+                            }
+                            if ((board[to] & COLOR_MASK) === nextTurn) {
+                                addMove(to, 0, 0);
+                            }
+                        }
+                        break;
                     case KING:
-                        {
-                            const movements = PIECE_MOVEMENTS[basePiece];
-                            for (const delta of movements) {
-                                const to = pos + delta;
-                                if (board[to] === EMPTY || (board[to] & COLOR_MASK) === nextTurn) {
-                                    addMove(to, 0);
-                                }
+                        for (const delta of KING_MOVEMENTS) {
+                            const to = pos + delta;
+                            if (board[to] === EMPTY) {
+                                addMove(to, 0, thisPlys + 1);
+                            } else if ((board[to] & COLOR_MASK) === nextTurn) {
+                                addMove(to, 0, 0);
                             }
                         }
                         break;
@@ -114,5 +135,8 @@ export function generateMoves(board: Board): Array<Move> {
         pos += 2;
     }
 
-    return moves;
+    return {
+        moves,
+        count: length >> 3
+    };
 }
